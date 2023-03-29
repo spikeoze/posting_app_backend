ded4113f9335e7c0fe8c4063637ebe03102f5e4e
Mukhtar Amin <mukhtaaramiin@gmail.com>
2023-03-29 09:15:36 +0300

    changed cors origin

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:3000/", "https://posting-app-seven.vercel.app/"],
     methods: "GET, POST, DELETE, PUT",
     credentials: true,
   })
